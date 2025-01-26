8ec20eef43ee0b9b659750584641e3870d2295ad
Kehinde Yishawu <fkennyf@gmail.com>
2025-01-26 15:08:34 +0100

    email setup testing11

--- backend/routes/mail.js
+++ backend/routes/mail.js
@@ -55,7 +55,7 @@ let generateRegFormAttachments = ({files})=>{
 
 export let mailRegFormData = async (inputs)=>{
     try {
-        let emailReport = zoho.sendMail({
+        let emailReport = await zoho.sendMail({
             from: "Fnmilove Academy sales@fnmiloveacademy.com",
             to: "vincent@fnmiloveacademy.com",
             subject: "Registration Form Applicant",
